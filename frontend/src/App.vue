8fd49ed8fcd2a35aec74e8b8a5f6a3b4a3f2802c
Eurakey <747417582@qq.com>
2025-10-31 14:27:39 +0800

    fix: Add Naive UI message provider

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,9 +1,13 @@
 <template>
-  <router-view />
+  <n-message-provider>
+    <router-view />
+  </n-message-provider>
 </template>
 
 <script setup>
+import { NMessageProvider } from 'naive-ui'
 // Main app component - uses router-view to render pages
+// Wrapped with NMessageProvider to enable useMessage() composable
 </script>
 
 <style>
